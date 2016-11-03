93d4ea638de9fc054fc1b615713913d17ff073a0
Stefanie <swift.stefanie@gmail.com>
2016-11-02 23:03:07 -0700

    updated to make api calls functional

--- app/services/flightCalculator.rb
+++ app/services/flightCalculator.rb
@@ -1,13 +1,75 @@
 require 'net/http'
+require 'uri'
+require 'json'
 
-url = URI.parse('http://partners.api.skyscanner.net/apiservices/pricing/v1.0')
-req = Net::HTTP::Post.new(url.to_s)
-res = Net::HTTP.start(url.host, url.port) {|http|
-  http.request(req)
+def createSkyscannerSession(params)
+	url = URI("http://partners.api.skyscanner.net/apiservices/pricing/v1.0?apiKey=" << ENV['skyscanner'])
+
+	http = Net::HTTP.new(url.host, url.port)
+
+	request = Net::HTTP::Post.new(url)
+	request["content-type"] = 'application/x-www-form-urlencoded'
+	request["accept"] = 'application/json'
+	request["cache-control"] = 'no-cache'
+	request.body = "OriginPlace=#{params['departure_airport']}-iata&Country=#{params['country']}&Currency=#{params['currency']}&DestinationPlace=#{params['destination']}-iata&OutboundDate=#{params['outbound_date']}&Locale=#{params['locale']}"
+	response = http.request(request)
+
+	if response.code != "201"
+	  session_id = nil
+	else
+	  polling_url = response["location"]
+	  session_id = polling_url.split('/').last
+
+	  return session_id
+	end
+end
+
+def getFlightPrices(params)
+	url = URI("http://partners.api.skyscanner.net/apiservices/pricing/v1.0/#{params['sessionKey']}?apiKey=prtl6749387986743898559646983194")
+
+	http = Net::HTTP.new(url.host, url.port)
+
+	request = Net::HTTP::Get.new(url)
+	request["content-type"] = 'application/x-www-form-urlencoded'
+	request["accept"] = 'application/json'
+	request["cache-control"] = 'no-cache'
+	request.body = ""
+	puts request.body
+	response = http.request(request)
+	puts response
+	puts response.body
+
+	if response.code != "201"
+	  session_id = nil
+	else
+	  polling_url = response["location"]
+	  session_id = polling_url.split('/').last
+
+	  return session_id
+	end
+end	
+
+theParams = {
+	"departure_airport" => "sfo",
+	"country" => "US",
+	"currency" => "USD",
+	"locale" => "en-US",
+	"destination" => "bdl",
+	"outbound_date" => "2016-12-16",
+	"inbound_date" => "2016-12-26",
+	"cabin_class" => "Economy",
+	"adult" => 1,
+	"children" => 0,
+	"infants" => 0,
 }
 
-puts res.header
+sessionKey = createSkyscannerSession(theParams)
 
+flightParams = {
+	"sessionKey" => sessionKey,
+}
+
+puts getFlightPrices(flightParams)
 
 
 #TODO this is done in the front end
