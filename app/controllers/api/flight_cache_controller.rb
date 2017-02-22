d0ba605d4dd5a7b93213a106f94dae897cf19d07
Stefanie Swift <swift.stefanie@gmail.com>
2017-02-22 08:53:55 -0800

    created cache api controller and started updating the flight model

---
+++ app/controllers/api/flight_cache_controller.rb
@@ -0,0 +1,18 @@
+class Api::FlightCacheController  < ApplicationController
+  def get_location_cache
+    traveler = params[:traveler]
+
+  end
+
+  def create_cache
+
+  end
+
+  def delete_cache
+
+  end
+
+  def update_cache
+    
+  end
+end
--- app/models/flight_cache.rb
+++ app/models/flight_cache.rb
@@ -1,2 +1,12 @@
 class FlightCache < ApplicationRecord
+
+  def get_destination_cache(starting_location, starting_type, price = nil, starting_date = nil, ending_date = nil)
+
+  end
+
+  def get_live_pricing(starting_locations, ending_locations, starting_date, ending_date)
+
+  end
+
+
 end
